d10dbd41eb6654852cb6c12cd885f0683f0052b0
Nikolaj Sørensen <rambo3889@gmail.com>
2020-08-14 22:39:45 +0200

    Added remaining tests for conversion service stuff.

--- Running-App/src/app/services/conversion.service.spec.ts
+++ Running-App/src/app/services/conversion.service.spec.ts
@@ -4,6 +4,7 @@ import { ConversionService } from './conversion.service';
 import { Distance, DistanceUnit } from '../shared/models/distance.model';
 import { Pace } from '../shared/models/pace.model';
 import { TimeUnit } from '../shared/models/timeunit.model';
+import { Timespan } from '../shared/models/timespan.model';
 
 describe('ConversionService', () => {
   let service: ConversionService;
@@ -56,11 +57,67 @@ describe('ConversionService', () => {
     expect(distanceInUnit).toEqual(12500);
  });
 
-
   it("Convert pace  990m/h to the distance in meters", () =>{
     let originalPace = new Pace(990, TimeUnit.Minute, DistanceUnit.M);
     let distanceInUnit = service.convertSpeedUnit(originalPace, DistanceUnit.KM);
     expect(distanceInUnit).toEqual(0.99);
   });
 
+  it("Convert pace 990m/h to the distance in meters", () =>{
+    let originalDistance = 990;
+    let originalPace = new Pace(originalDistance, TimeUnit.Minute, DistanceUnit.M);
+    let resultingDistance = service.convertSpeedUnit(originalPace, DistanceUnit.M);
+    expect(resultingDistance).toEqual(originalDistance);
+  });
+
+  //Time conversions
+  it("Convert a full day to seconds", () => {
+    let fullDay = new Timespan(1,0,0,0);
+    let allInSeconds = service.convertTimeStampToUnitNumber(fullDay, TimeUnit.Second);
+    let expectedSeconds = 24 * 60 * 60;
+    expect(allInSeconds).toEqual(expectedSeconds);
+  });
+
+  it("Convert a full day to minutes", () => {
+    let fullDay = new Timespan(1,0,0,0);
+    let allInMinutes = service.convertTimeStampToUnitNumber(fullDay, TimeUnit.Minute);
+    let expected = 24 * 60;
+    expect(allInMinutes).toEqual(expected);
+  });
+
+  it("Convert a full day to Hours", () => {
+    let fullDay = new Timespan(1,0,0,0);
+    let result = service.convertTimeStampToUnitNumber(fullDay, TimeUnit.Hour);
+    let expected = 24;
+    expect(result).toEqual(expected);
+  });
+
+  it("Convert a full day to Hours", () => {
+    let fullDay = new Timespan(1,0,0,0);
+    let result = service.convertTimeStampToUnitNumber(fullDay, TimeUnit.Day);
+    let expected = 1;
+    expect(result).toEqual(expected);
+  });
+
+  it("Convert 1.5 day to Hours", () => {
+    let fullDay = new Timespan(1,12,0,0);
+    let result = service.convertTimeStampToUnitNumber(fullDay, TimeUnit.Day);
+    let expected = 1.5;
+    expect(result).toEqual(expected);
+  });
+
+
+  it("Convert 2days-1 second to seconds", () => {
+    let fullDay = new Timespan(1,23,59,59);
+    let result = service.convertTimeStampToUnitNumber(fullDay, TimeUnit.Second);
+    let expected = ((24 * 60 * 60) * 2) - 1;
+    expect(result).toEqual(expected);
+  });
+
+  it("Convert 2days-1 minute to minute", () => {
+    let fullDay = new Timespan(1,23,59,0);
+    let result = service.convertTimeStampToUnitNumber(fullDay, TimeUnit.Minute);
+    let expected = ((24 * 60 ) * 2) - 1;
+    expect(result).toEqual(expected);
+  });
 });
